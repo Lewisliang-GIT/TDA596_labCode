9e44cbd640059ee1fbd52b9eb457b7e72b59a15f
Lanyu Liang <llyxiamen@hotmail.com>
2023-10-31 14:14:38 +0100

    basic server/client

--- lab1/server.go
+++ lab1/server.go
@@ -1,16 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
-	"net/http"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi %s", r.URL.Path[1:])
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide port number")
+		return
+	}
+
+	PORT := ":" + arguments[1]
+	l, err := net.Listen("tcp", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer l.Close()
+
+	c, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if strings.TrimSpace(string(netData)) == "STOP" {
+			fmt.Println("Exiting TCP server!")
+			return
+		}
+
+		fmt.Print("-> ", string(netData))
+		t := time.Now()
+		myTime := t.Format(time.RFC3339) + "\n"
+		c.Write([]byte(myTime))
+	}
 }
