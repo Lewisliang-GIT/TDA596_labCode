73c4229c01fe9c60eb58ed124fa6cff5e0d38714
Lanyu Liang <llyxiamen@hotmail.com>
2023-12-02 21:27:22 +0100

    lab3

--- lab3/unit/node.go
+++ lab3/unit/node.go
@@ -1,6 +1,8 @@
 package unit
 
 import (
+	"crypto/sha1"
+	"math/big"
 	"sync"
 )
 
@@ -17,6 +19,60 @@ type Node struct {
 	Bucket map[Key]string
 	Mutex  sync.Mutex
 }
+
+func hashString(elt string) *big.Int {
+	hasher := sha1.New()
+	hasher.Write([]byte(elt))
+	return new(big.Int).SetBytes(hasher.Sum(nil))
+}
+
+const keySize = sha1.Size * 8
+
+var two = big.NewInt(2)
+var hashMod = new(big.Int).Exp(big.NewInt(2), big.NewInt(keySize), nil)
+
+func jump(address string, fingerentry int) *big.Int {
+	n := hashString(address)
+	fingerentryminus1 := big.NewInt(int64(fingerentry) - 1)
+	jump := new(big.Int).Exp(two, fingerentryminus1, nil)
+	sum := new(big.Int).Add(n, jump)
+
+	return new(big.Int).Mod(sum, hashMod)
+}
+
+func between(start, elt, end *big.Int, inclusive bool) bool {
+	if end.Cmp(start) > 0 {
+		return (start.Cmp(elt) < 0 && elt.Cmp(end) < 0) || (inclusive && elt.Cmp(end) == 0)
+	} else {
+		return start.Cmp(elt) < 0 || elt.Cmp(end) < 0 || (inclusive && elt.Cmp(end) == 0)
+	}
+}
+
+n.find_successor(id)
+if (id ∈ (n, successor])
+return true, successor;
+else
+return false, closest_preceding_node(id);
+
+// search the local table for the highest predecessor of id
+n.closest_preceding_node(id)
+// skip this loop if you do not have finger tables implemented yet
+for i = m downto 1
+if (finger[i] ∈ (n,id])
+return finger[i];
+return successor;
+
+// find the successor of id
+find(id, start)
+found, nextNode = false, start;
+i = 0
+while not found and i < maxSteps
+found, nextNode = nextNode.find_successor(id);
+i += 1
+if found
+return nextNode;
+else
+report error;
 
 func (n *Node) join(joinNode *Node) error {
 
